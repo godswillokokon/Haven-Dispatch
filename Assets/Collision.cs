8fdd88dd4f9e998375475020e9163f6833e901cb
Godswill <godswilll@analogrepublic.com>
2022-06-23 17:01:09 +0100

    bump and log

---
+++ Assets/Collision.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+
+{
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+      UnityEngine.Debug.Log("some text");
+   
+
+    }
+}
