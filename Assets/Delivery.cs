9dd819c8f61bc5c564a6e50994240ad3ea4326e2
Godswill <godswilll@analogrepublic.com>
2022-06-28 14:23:35 +0100

    change color of car real time when it picks up a package

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -6,9 +6,16 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 
 {
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
     [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage;
+    SpriteRenderer spriteRenderer;
 
+    void Start() {
+
+      spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     void OnCollisionEnter2D(Collision2D other) 
     {
       // Debug.Log("Bump!");
@@ -21,6 +28,7 @@ public class Delivery : MonoBehaviour
         {
           Debug.Log("Package picked up");
           hasPackage = true;
+          spriteRenderer.color = hasPackageColor;
           Destroy(other.gameObject, destroyDelay);
         }
 
@@ -28,6 +36,7 @@ public class Delivery : MonoBehaviour
         {
           Debug.Log("Package Delivered");
            hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
